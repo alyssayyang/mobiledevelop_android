448f0c6f37c7506acbdb2cd771c7e8f728feab32
YANG YANG <yang.yang16@northeastern.edu>
2022-05-13 17:24:37 -0400

    A1 initial commit

---
+++ app/src/main/java/edu/neu/numad22sp_yangyang/MainActivity.java
@@ -0,0 +1,39 @@
+/*
+YANG YANG
+CS 5520 A1
+MAY 13 2022
+ */
+
+package edu.neu.numad22sp_yangyang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void intromyself(View v){
+        displayToastMsg("YANG YANG \nyang.yang16@northeastern.edu");
+    }
+
+    public void displayToastMsg(String msg){
+        Toast toast = Toast.makeText(this,msg,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+        toast.show();
+    }
+}
