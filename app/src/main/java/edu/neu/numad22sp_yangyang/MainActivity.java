e7ba185afa3c605f204da38982763960c72cc22b
YANG YANG <yang.yang16@northeastern.edu>
2022-05-15 11:09:35 -0400

    A1 initial commit

--- app/src/main/java/edu/neu/numad22sp_yangyang/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_yangyang/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void intromyself(View v){
+    public void intro(View v){
         displayToastMsg("YANG YANG \nyang.yang16@northeastern.edu");
     }
 
